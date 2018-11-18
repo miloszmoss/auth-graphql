a98b9896f657878248cac8d6445ad48c8d69acc7
miloszmoska <milosz.moss@gmail.com>
2018-11-18 13:19:25 +0100

    require auth with hoc, and redirect

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -4,14 +4,24 @@ import { graphql } from 'react-apollo';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      errors: '',
+      errors: [],
     };
   }
+
+  componentWillUpdate(nextProps) {
+    //this.props => the old, current set of props
+    //nextProps => the next set of props that will be in place when component rerenders
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard');
+    }
+  }
+
   onSubmit({ email, password }) {
     this.props
       .mutate({
@@ -30,10 +40,10 @@ class LoginForm extends Component {
     return (
       <div>
         <h3>Login</h3>
-        <AuthForm onSubmit={this.onSubmit.bind(this)} />
+        <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(graphql(mutation)(LoginForm));
---
+++ client/components/requireAuth.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import currentUserQuery from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
+
+export default WrappedComponent => {
+  class requireAuth extends Component {
+    componentWillUpdate(nextProps) {
+      if (!nextProps.data.loading && !nextProps.data.user) {
+        hashHistory.push('/login');
+      }
+    }
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+  graphql(currentUserQuery)(requireAuth);
+};
